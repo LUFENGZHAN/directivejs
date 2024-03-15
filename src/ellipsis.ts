bdc19e67f7028a1102443e3f5cdefee7b4255e33
LUFENGZHAN <xiaozhan2000423@163.com>
2024-03-15 23:54:45 +0800

    init

--- src/ellipsis.ts
+++ src/ellipsis.ts
@@ -1,16 +1,18 @@
 import { CustomDirectiveBinding } from './index'
 export default {
-    mounted(el: any, binding: CustomDirectiveBinding) {
+    mounted(el: { style: any; }, binding: CustomDirectiveBinding) {
         const webkitLineClamp = typeof Number(binding.value) === 'number' && Number(binding.value) || 1
-        el.style.display = "-webkit-box";
-        el.style.webkitLineClamp = webkitLineClamp;
-        el.style.webkitBoxOrient = "vertical";
-        el.style.maxWidth = "100%";
-        el.style.height = "auto";
-        el.style.lineHeight = "1.4";
-        el.style.overflow = "hidden";
-        el.style.textOverflow = "ellipsis";
-        el.style.wordWrap = 'break-word';
-        el.style.wordBreak = "break-all";
+        Object.assign(el?.style, {
+            display: "-webkit-box",
+            webkitLineClamp: webkitLineClamp,
+            webkitBoxOrient: "vertical",
+            maxWidth: "100%",
+            height: "auto",
+            lineHeight: "1.4",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            wordWrap: "break-word",
+            wordBreak: "break-all",
+        });
     }
 }
--- src/sizeOb.ts
+++ src/sizeOb.ts
@@ -1,17 +1,18 @@
-import {CustomDirectiveBinding} from './index'
+import { CustomDirectiveBinding } from './index'
 
 let map = new WeakMap()
 let ob = new ResizeObserver(entries => {
-   for(const entry of entries){
-    const hand = map.get(entry.target)
-    hand && hand({
-      width:entry.borderBoxSize[0].inlineSize,
-      height:entry.borderBoxSize[0].blockSize
-    })
-   }
+    for (const entry of entries) {
+        const hand = map.get(entry.target)
+        hand && hand({
+            width: entry.borderBoxSize[0].inlineSize,
+            height: entry.borderBoxSize[0].blockSize
+        })
+    }
 })
 export default {
     mounted(el: Element, binding: CustomDirectiveBinding) {
+        if (typeof binding.value !== 'function' && Object.prototype.toString.call(binding.value) !== '[object Function]') return console.warn('binding value must be a function')
         map.set(el, binding.value)
         ob.observe(el)
     },
